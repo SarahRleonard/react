b7f1071f14f8461033266d14ca30ce03a0f0c17a
MademoiselleRolleo <mademoiselle.rolleo@gmail.com>
2023-02-08 13:16:10 -0500

    getting ready for css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
 import Employee from './components/Employee';
+import { useState } from 'react';
 
 function App() {
+  const [role, setRole] = useState('dev');
   const showEmployees = true;
   return (
     <div className="App">
       {showEmployees ? (
         <>
-          <Employee />
-          <Employee />
-          <Employee />
+          <input
+            type='text'
+            onChange={(e) => {
+              console.log(e.target.value)
+              setRole(e.target.value);
+            }} />
+          <Employee name="Caleb" role="intern" />
+          <Employee name="Abby" role={role} />
+          <Employee name="John" />
         </>
 
       ) : (<p>You are not authorized</p>
