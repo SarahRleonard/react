3bbfe5a69df0e9148c3f150fe65a64abcf88ed3a
MademoiselleRolleo <mademoiselle.rolleo@gmail.com>
2023-02-14 07:41:58 -0500

    save before switch computer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import './index.css';
 import Employee from './components/Employee';
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [role, setRole] = useState('dev');
+  const [employees, setEmployees] = useState(
+    [
+      { name: 'Caleb', role: 'Developer', img: 'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg' },
+      { name: 'Mousse', role: 'Gate keeper', img: 'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg' },
+      { name: 'Mila', role: 'Hugger', img: 'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg' },
+      { name: 'Ashley', role: 'Manager', img: 'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg' },
+      { name: 'Laure', role: 'Developer', img: 'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg' },
+      { name: 'Julia', role: 'Boss', img: 'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg' }
+    ]
+  );
   const showEmployees = true;
   return (
     <div className="App">
@@ -15,9 +26,14 @@ function App() {
               console.log(e.target.value)
               setRole(e.target.value);
             }} />
-          <Employee name="Caleb" role="intern" />
-          <Employee name="Abby" role={role} />
-          <Employee name="John" />
+          <div className="flex flex-wrap justify-center">
+            {employees.map((employee) => {
+              console.log(employee)
+              return (
+                <Employee key={uuidv4()} name={employee.name} role={employee.role} img={employee.img} />
+              );
+            })}
+          </div>
         </>
 
       ) : (<p>You are not authorized</p>
