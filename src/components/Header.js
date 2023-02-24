872487e559f2fa880b6da4d24c031586fb6012a8
Sarah Leonard <sarahleonard@MBP-de-Sarah-2.hitronhub.home>
2023-02-24 15:26:52 -0500

    setup change of color for the active tabs of the navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { NavLink } from "react-router-dom";
 
 const navigation = [
-  { name: "Employees", href: "/employees", current: true },
-  { name: "Customers", href: "/customers", current: false },
-  { name: "Projects", href: "#", current: false },
-  { name: "Calendar", href: "#", current: false },
+  { name: "Employees", href: "/employees" },
+  { name: "Customers", href: "/customers" },
+  { name: "Projects", href: "other" },
+  { name: "Calendar", href: "other2" },
 ];
 
 function classNames(...classes) {
@@ -35,19 +36,21 @@ export default function Header(props) {
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
-                      <a
+                      <NavLink
                         key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current
-                            ? "no-underline bg-gray-900 text-white"
-                            : "no-underline text-gray-300 hover:bg-gray-700 hover:text-white",
-                          "px-3 py-2 rounded-md text-sm font-medium"
-                        )}
-                        aria-current={item.current ? "page" : undefined}
+                        to={item.href}
+                        className={({ isActive }) => {
+                          return (
+                            "px-3 py-2 rounded-md text-sm font-medium" +
+                            (!isActive
+                              ? "text-gray-300 hover:bg-gray-700 hover:text-white"
+                              : "bg-gray-900 text-white")
+                          );
+                          /* console.log(item.href + ' ' + isActive) */
+                        }}
                       >
                         {item.name}
-                      </a>
+                      </NavLink>
                     ))}
                   </div>
                 </div>
@@ -143,6 +146,5 @@ export default function Header(props) {
         </>
       )}
     </Disclosure>
-    
   );
 }
